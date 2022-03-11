8372a9c9487c19e2d63240a8c59cd2a7360d2f8d
Marco Flores <marcoflores1029@gmail.com>
2022-03-11 15:36:18 -0800

    removed itemSeeds.json didn't need it, and finished seed.js to run the seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,6 +1,36 @@
 const db = require("../config/connection");
-const { Item, Restaurant, Review, User} = require("../models");
+const { Restaurant, Review, User } = require("../models");
+
 const reviewSeeds = require("./reviewSeeds.json");
 const restaurantSeeds = require("./restaurantSeeds.json");
-const itemSeeds = require("./itemSeeds.json");
 const userSeeds = require("./userSeeds.json");
+
+db.once("open", async () => {
+  // clean database
+    try {
+        await User.deleteMany({});
+        await Restaurant.deleteMany({});
+        await Review.deleteMany({});
+
+        // bulk create each model
+        await User.insertMany(userSeeds);
+        const reviews = await Review.insertMany(reviewSeeds);
+        const restaurants = await Restaurant.insertMany(restaurantSeeds);
+
+        for (const rev of reviews) {
+            // randomly add each Reviews to a Restaurant
+            const tempRestaurant =
+            restaurants[Math.floor(Math.random() * restaurants.length)];
+            tempRestaurant.reviews.push(rev._id);
+        }
+        
+        for (const rest of restaurants) {
+            await rest.save();
+        }
+
+        console.log("all done!");
+        process.exit(0);
+    }   catch (err) {
+        throw err;  
+    }
+});
