85242da1193e51c458c1a2bf0c1429367d476296
Luka Licheli <lukalicheli@gmail.com>
2022-03-12 17:40:25 -0800

    Created Footer.js and styled it

---
+++ client/src/components/Footer.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Footer() {
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+      <div>
+
+    <BottomNavigation sx={{position: 'fixed', bottom: 0, left: 0, right: 0, width: 500 }} elevation={3} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Search"
+        value="search"
+        icon={<SearchIcon />}
+      />
+      <BottomNavigationAction
+        label="Favorites"
+        value="favorites"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="Menu"
+        value="menu"
+        icon={<MenuIcon />}
+      />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
+    </div>
+  );
+}
--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 export default function UserProfile() {
     return (
+        <div>
         <h1 className='text-center'>User Profile Page</h1>
+        <Footer />
+        </div>
     );
 }
