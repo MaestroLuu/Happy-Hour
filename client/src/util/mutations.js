47cfc97568350c79067e3a14264e717cd4514602
Marco Flores <marcoflores1029@gmail.com>
2022-03-12 22:27:23 -0800

    maybe sovled the addReview? commented out client/src/utils/queries and mutations cause page won't load

--- client/src/util/mutations.js
+++ client/src/util/mutations.js
@@ -29,80 +29,80 @@ mutation login($email: String!, $password: String!) {
 }
 `;
 
-// USER PROFILE
-export const ADD_FAVORITE_RESTAURANT = gql`
-mutation addFavoriteRestaurant($restaurantId: ID!) {
-  addFavoriteRestaurant(restaurantId: $restaurantId) {
-    favoriteRestaurants {
-      _id
-      restaurantName
-      address
-      happyHours
-      foodSpecial
-      drinkSpecial
-    }
-  }
-}
-`;
+// // USER PROFILE
+// export const ADD_FAVORITE_RESTAURANT = gql`
+// mutation addFavoriteRestaurant($restaurantId: ID!) {
+//   addFavoriteRestaurant(restaurantId: $restaurantId) {
+//     favoriteRestaurants {
+//       _id
+//       restaurantName
+//       address
+//       happyHours
+//       foodSpecial
+//       drinkSpecial
+//     }
+//   }
+// }
+// `;
 
-// USER PROFILE
-export const REMOVE_FAVORITE_RESTAURANT = gql`
-mutation removeFavoriteRestaurant($restaurantId: ID!) {
-  removeFavoriteRestaurant(restaurantId: $restaurantId) {
-    favoriteRestaurants {
-      _id
-      restaurantName
-      address
-      happyHours
-      foodSpecial
-      drinkSpecial
-    }
-  }
-}
-`;
+// // USER PROFILE
+// export const REMOVE_FAVORITE_RESTAURANT = gql`
+// mutation removeFavoriteRestaurant($restaurantId: ID!) {
+//   removeFavoriteRestaurant(restaurantId: $restaurantId) {
+//     favoriteRestaurants {
+//       _id
+//       restaurantName
+//       address
+//       happyHours
+//       foodSpecial
+//       drinkSpecial
+//     }
+//   }
+// }
+// `;
 
-// RESTAURANT PAGE
-export const ADD_REVIEW = gql`
-mutation addReview($restaurantId: ID!, $reviewText: String!) {
-  addReview(restaurantId: $restaurantId, reviewText: $reviewText) {
-    _id
-    restaurantName
-    reviews {
-      _id
-      reviewAuthor
-      reviewText
-      createdAt
-      stars
-    }
-  }
-}
-`;
+// // RESTAURANT PAGE
+// export const ADD_REVIEW = gql`
+// mutation addReview($restaurantId: ID!, $reviewText: String!) {
+//   addReview(restaurantId: $restaurantId, reviewText: $reviewText) {
+//     _id
+//     restaurantName
+//     reviews {
+//       _id
+//       reviewAuthor
+//       reviewText
+//       createdAt
+//       stars
+//     }
+//   }
+// }
+// `;
 
-// RESTAURANT PAGE
-export const DELETE_REVIEW = gql`
-mutation deleteReview($restaurantId: ID!, $reviewText: String!) {
-  deleteReview(restaurantId: $restaurantId, reviewText: $reviewText) {
-    _id
-    restaurantName
-    reviews {
-      _id
-    }
-  }
-}
-`;
+// // RESTAURANT PAGE
+// export const DELETE_REVIEW = gql`
+// mutation deleteReview($restaurantId: ID!, $reviewText: String!) {
+//   deleteReview(restaurantId: $restaurantId, reviewText: $reviewText) {
+//     _id
+//     restaurantName
+//     reviews {
+//       _id
+//     }
+//   }
+// }
+// `;
 
-// RESTAURANT PAGE
-export const UPDATE_REVIEW = gql`
-mutation addReview($restaurantId: ID!, $reviewText: String!) {
-  addReview(restaurantId: $restaurantId, reviewText: $reviewText) {
-    _id
-    restaurantName
-    reviews {
-      _id
-      reviewText
-      createdAt
-      stars
-    }
-  }
-}
-`;
+// // RESTAURANT PAGE
+// export const UPDATE_REVIEW = gql`
+// mutation addReview($restaurantId: ID!, $reviewText: String!) {
+//   addReview(restaurantId: $restaurantId, reviewText: $reviewText) {
+//     _id
+//     restaurantName
+//     reviews {
+//       _id
+//       reviewText
+//       createdAt
+//       stars
+//     }
+//   }
+// }
+// `;
--- client/src/util/queries.js
+++ client/src/util/queries.js
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_ME = gql`
+export const ME = gql`
+query me {
   me {
     favoriteRestaurants {
       _id
@@ -11,40 +12,41 @@ export const QUERY_ME = gql`
       drinkSpecial
     }
   }
+}
 `;
 
-export const QUERY_SINGLE_RESTAURANT = gql`
-restaurant(id: $restaurantId) {
-  _id
-  restaurantName
-  address
-  drinkSpecial
-  foodSpecial
-  foodType
-  happyHours
-  items {
-    _id
-    itemName
-    price
-  }
-  reviews {
-    _id
-    reviewAuthor
-    reviewText
-    createdAt
-    stars
-  }
-`;
+// export const QUERY_SINGLE_RESTAURANT = gql`
+// restaurant(id: $restaurantId) {
+//   _id
+//   restaurantName
+//   address
+//   drinkSpecial
+//   foodSpecial
+//   foodType
+//   happyHours
+//   items {
+//     _id
+//     itemName
+//     price
+//   }
+//   reviews {
+//     _id
+//     reviewAuthor
+//     reviewText
+//     createdAt
+//     stars
+//   }
+// `;
 
-export const QUERY_ALL_RESTAURANTS = gql`
-  restaurants {
-    _id
-    restaurantName
-    address
-    zipCode
-    happyHours
-    foodSpecial
-    drinkSpecial
-  }
-`;
+// export const QUERY_ALL_RESTAURANTS = gql`
+//   restaurants {
+//     _id
+//     restaurantName
+//     address
+//     zipCode
+//     happyHours
+//     foodSpecial
+//     drinkSpecial
+//   }
+// `;
 
