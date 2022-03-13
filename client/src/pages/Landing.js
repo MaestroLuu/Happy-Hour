39ccda640e031bbc061ccdeebce02091818874dd
Luka Licheli <lukalicheli@gmail.com>
2022-03-13 13:04:06 -0700

    Designed splash page and 2 images

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Footer from "../components/Footer";
+
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Beers from '../components/beers2.jpg';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Landing() {
-    return (
-        <h1 className='text-center'>Landing (Splash) Page</h1>
-    );
+  return (
+    <div>
+      <h1 style={{textAlign: "center", fontSize: "45px"}}>Happy Hour</h1>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="250"
+          image={Beers}
+          alt="beers"
+        />
+      </Card>
+      <h3 style={{textAlign: "center", marginTop: "50px", fontSize: "25px"}}>For local happy hour <br/>information type in your <br/>zipcode below.</h3>
+      <TextField sx={{ maxWidth: 300, marginTop: "50px"}} style={{display: "flex", justifyContent: "center"}} id="outlined-search" label="Search By Zipcode" type="search" />
+      <Button sx={{ maxWidth: 100 }} variant="outlined" size="large">
+          Submit
+        </Button>
+      
+      <Footer />
+    </div>
+  );
 }
