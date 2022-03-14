130b4044e65075a640f6360589e0d5f0af4f8e13
Luka Licheli <lukalicheli@gmail.com>
2022-03-14 11:20:24 -0700

    Added filter component and styled homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,32 @@
 import { useAuth } from "../util/auth";
+import TextField from '@mui/material/TextField';
+import Footer from "../components/Footer";
+import Cards from "../components/Cards";
+import Filter from "../components/Filter";
+import Box from '@mui/material/Box';
 
 export default function Home() {
   const { isLoggedIn, user } = useAuth();
   return (
     <div>
       {/* TODO: display logged in user's username */}
-      <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
+      {/* <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1> */}
+ 
+      <TextField sx={{ maxWidth: 300, marginTop: "50px", mx: "auto"}} style={{display: "flex", justifyContent: "center"}} id="outlined-search" label="Search By Zipcode" type="search" />
+      <Box sx={{mx: "auto"}}>
+        <Filter />
+      </Box>
       <hr />
-      <p>
-      All restaurants are displayed here.
-      </p>
+
+      <Cards />
+      <br />
+      <Cards />
+      <br />
+      <Cards />
+      <br />
+
+
+      <Footer />
     </div>
   );
 }
