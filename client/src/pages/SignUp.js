ec79569e1328960390862ec0213a9cb8026cc61b
Luka Licheli <lukalicheli@gmail.com>
2022-03-14 23:15:37 -0700

    Signup page, tested email and username fields

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../util/auth";
-import MailIcon from "@mui/icons-material/Mail";
-import "./SignUp.css";
 
-import * as React from "react";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import IconButton from "@mui/material/IconButton";
-import VpnKeyIcon from "@mui/icons-material/VpnKey";
-
+import FormControl from "@mui/material/FormControl";
 import Input from "@mui/material/Input";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import { NavLink } from "react-router-dom";
-import Box from '@mui/material/Box';
+import MailIcon from "@mui/icons-material/Mail";
+
 // This signup form is intentionally minimalist to reduce effort required to
 // customize it to your app's needs. See the excellent best practices guide for
 // sign informs on web.dev https://web.dev/sign-in-form-best-practices/
 
 // TODO: customize styles or import styles with favorite css approach
+const styles = {
+  formControl: {
+    display: "flex",
+    padding: "0.25em",
+  },
+  label: {
+    flex: "0 1 6em",
+    paddingRight: "0.25em",
+  },
+};
 
 const initialFormState = {
   username: "",
@@ -30,27 +32,6 @@ const initialFormState = {
 };
 
 export default function SignUp() {
-  const [values, setValues] = React.useState({
-    amount: "",
-    password: "",
-    weight: "",
-    weightRange: "",
-    showPassword: false,
-  });
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  /////////////////////////////////////////////////
-
   const { isLoggedIn, signup, loading, error } = useAuth();
   const [formState, setFormState] = useState(initialFormState);
 
@@ -73,53 +54,25 @@ export default function SignUp() {
 
   if (isLoggedIn) {
     // navigate to the home page
-    return <Navigate to="/home" replace />;
+    return <Navigate to="/" replace />;
   }
   return (
-    <Box
-    sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      p: 1,
-      m: 1,
-      bgcolor: 'background.paper',
-      borderRadius: 1,
-    }}>
-  
     <div>
-      <h1 className="signup-center">Sign Up</h1>
+      <h1>Sign Up</h1>
+      <hr />
       <form onSubmit={handleSubmit}>
-        {/*////////////////////////////////////////////////////////////////////////*/}
-
-        <div>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel>
-            <Input
-              disabled={loading}
-              id="input-with-icon-adornment"
-              placeholder="Type your email"
-              value={formState.email.value}
-              onChange={handleInputChange}
-              startAdornment={
-                <InputAdornment position="start">
-                  <MailIcon />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </div>
-
-        {/*////////////////////////////////////////////////////////////////////////*/}
-
         <div>
           <FormControl variant="standard">
             <InputLabel htmlFor="input-with-icon-adornment">
               Username
             </InputLabel>
             <Input
+              autoFocus
               disabled={loading}
-              id="input-with-icon-adornment"
+              id="username"
+              type="text"
               placeholder="Type your username"
+              name="username"
               value={formState.username.value}
               onChange={handleInputChange}
               startAdornment={
@@ -131,54 +84,51 @@ export default function SignUp() {
           </FormControl>
         </div>
 
-        {/*////////////////////////////////////////////////////////////////////////*/}
-
-        <div>
-          <FormControl  variant="standard">
-            <InputLabel htmlFor="standard-adornment-password">
-              Password
+        <div style={styles.formControl}>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Email
             </InputLabel>
             <Input
-              id="standard-adornment-password"
-              placeholder="Type your password"
-              type={values.showPassword ? "text" : "password"}
-              value={formState.password.value}
+              disabled={loading}
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={formState.email.value}
               onChange={handleInputChange}
               startAdornment={
                 <InputAdornment position="start">
-                  <VpnKeyIcon />
-                </InputAdornment>
-              }
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
+                  <MailIcon />
                 </InputAdornment>
               }
             />
           </FormControl>
         </div>
 
-        {/*////////////////////////////////////////////////////////////////////////*/}
-
-        <div className="signup-button">
-          <button disabled={loading} type="submit">
-            {loading ? "Loading..." : "SIGN UP"}
-          </button>
+        <div style={styles.formControl}>
+          <label htmlFor="new-password" style={styles.label}>
+            Password
+          </label>
+          <input
+            disabled={loading}
+            id="new-password"
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={formState.password.value}
+            onChange={handleInputChange}
+          />
         </div>
-        <div>
-          <p className="member">Already a member?</p>
-          <NavLink to="/login" className="login">
-            Login
-          </NavLink>
+
+
+
+        <div style={styles.formControl}>
+          <button disabled={loading} type="submit">
+            {loading ? "Loading..." : "Submit"}
+          </button>
         </div>
       </form>
     </div>
-    </Box>
   );
 }
