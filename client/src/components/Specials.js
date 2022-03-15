827f8168a7ca927f32c887db40e7373ff5ed14b6
Luka Licheli <lukalicheli@gmail.com>
2022-03-14 20:41:27 -0700

    Updated specials

--- client/src/components/Specials.js
+++ client/src/components/Specials.js
@@ -1,24 +1,47 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { QUERY_SINGLE_RESTAURANT } from "../util/queries";
+import {useQuery } from "@apollo/client";
+import {useParams} from 'react-router-dom';
 
-export default function Drinks() {
+export default function Specials() {
+
+  const { restaurantId } = useParams("622fed32d1fd586b8296ad0c");
+
+
+  const { data } = useQuery(QUERY_SINGLE_RESTAURANT, {
+    variables: { restaurantId: restaurantId }
+    
+  });
+  console.log(data);
+
+  const restaurant = data?.restaurant || [];
+  console.log(restaurant);
+
   return (
-    <Card sx={{ maxWidth: 345, mx: "auto" }}>
-      <CardContent style={{textAlign: "center"}} >
-        <Typography style={{marginTop: "30px"}} gutterBottom variant="h5" component="div">
-          Modelo
-        </Typography>
-        <Typography gutterBottom variant="h5" component="div">
-          IPA
-        </Typography>
-        <Typography gutterBottom variant="h5" component="div">
-          Margarita 
-        </Typography>
-        
-      </CardContent>
+    <div>
+      
+        <Card key={restaurant._id} sx={{ maxWidth: 345, mx: "auto" }}>
+          <CardContent style={{ textAlign: "center" }}>
+            <Typography
+              style={{ marginTop: "30px" }}
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
+             {restaurant.item}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              IPA
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              Margarita
+            </Typography>
+          </CardContent>
+        </Card>
      
-    </Card>
+    </div>
   );
 }
