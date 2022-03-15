6548616c1eb535fe3790a7d22ce52c686ad65cff
Luka Licheli <lukalicheli@gmail.com>
2022-03-15 11:22:45 -0700

    Added menu items and added links to the buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,22 @@
 import React from "react";
-
 import Cards from "../components/Cards";
-
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-
-import Filter from "../components/Filter";
+import TextField from "@mui/material/TextField";
 import Footer from "../components/Footer";
 
-// import { useAuth } from "../util/auth";
-
 const Home = (props) => {
-  
-
-  // const { isLoggedIn, user } = useAuth();
   return (
     <div>
-      {/* TODO: display logged in user's username */}
-      {/* <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1> */}
-      <TextField sx={{ maxWidth: 300, marginTop: "50px", mx: "auto"}} style={{display: "flex", justifyContent: "center"}} id="outlined-search" label="Search By Zipcode" type="search" />
-      <Box sx={{mx: "auto"}}>
-        <Filter />
-      </Box>
-      
-      <Cards 
-      zipcode={props.zipcode}
+      <TextField
+        sx={{ maxWidth: 300, marginTop: "50px", mx: "auto" }}
+        style={{ display: "flex", justifyContent: "center" }}
+        id="outlined-search"
+        label="Search By Zipcode"
+        type="search"
       />
-
+      <Cards zipcode={props.zipcode} />
       <Footer />
     </div>
   );
-}
+};
 
 export default Home;
