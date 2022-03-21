e46769ed0846301a70db6e73abb581ae533c3f51
Marco Flores <marcoflores1029@gmail.com>
2022-03-21 10:47:15 -0700

    added properties for phoneNumber and webpage

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -1,30 +1,41 @@
 const { Schema, model } = require("mongoose");
 const itemSchema = require("./Item");
+const locationSchema = require("./Location");
 
-const restaurantSchema = new Schema (
-{
+const restaurantSchema = new Schema(
+  {
     restaurantName: {
-        type: String,
-        required: "You need a restaurant name.",
-        unique: true,
+      type: String,
+      required: "You need a restaurant name.",
+      unique: true,
     },
+    phoneNumber: {
+      type: Number,
+      unique: true,
+      minlength: 10,
+      maxlength: 11,
+    },
+    webpage: {
+      type: String,
+    },
+    location: [locationSchema],
     happyHours: {
-        type: String,
-        require: true,
+      type: String,
+      require: true,
     },
     items: [itemSchema],
     reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Review"
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
     ],
-},
-    {
-        toJSON: {
-            virtuals: true,
-        },
-    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
 );
 
 const Restaurant = model("Restaurant", restaurantSchema);
