508cd6df20439a50b50686b4c428dd4446a35c31
Luka Licheli <lukalicheli@gmail.com>
2022-03-26 21:24:59 -0700

    Changed color on footer buttons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,8 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import SearchIcon from "@mui/icons-material/Search";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Box } from "@mui/material";
-import LoginIcon from '@mui/icons-material/Login';
-import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useAuth } from "../util/auth";
 
 import { NavLink } from "react-router-dom";
@@ -28,59 +28,63 @@ export default function Footer() {
   const { isLoggedIn, logout } = useAuth();
   return (
     <div>
-      <br></br><br></br>
-    <Box sx={{ 
-      width: "100%", 
-      mx: "auto",
-      bottom: 0,
-      left: 0,
-      right: 0,
-      position: "fixed",
-      backgroundColor: "black"
-      }}
-      elevation={3}
+      <br></br>
+      <br></br>
+      <Box
+        sx={{
+          width: "100%",
+          mx: "auto",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          position: "fixed",
+          backgroundColor: "black",
+        }}
+        elevation={3}
       >
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={handleChange}
-        style={{backgroundColor: "transparent", display: "flex", justifyContent: "space-evenly"}}
-      >
-        <NavLink to="/restaurants">
-          <BottomNavigationAction
-            label="Search"
-            value="search"
-            icon={<SearchIcon />}
-          />
-        </NavLink>
-        <NavLink to="/user">
-          <BottomNavigationAction
-            label="Favorites"
-            value="favorites"
-            icon={<FavoriteIcon />}
-          />
-        </NavLink>
-        {isLoggedIn ? 
-        (
-          <NavLink to="/landing">
-          <BottomNavigationAction
-            label="logout"
-            value="logout"
-            icon={<LogoutIcon />}
-            onClick={logout}
-          />
-        </NavLink>
-        ):
-        (
-          <NavLink to="/login">
-          <BottomNavigationAction
-            label="login"
-            value="login"
-            icon={<LoginIcon />}
-          />
-        </NavLink>
-        )}
-      </BottomNavigation>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={handleChange}
+          style={{
+            backgroundColor: "transparent",
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <NavLink to="/restaurants">
+            <BottomNavigationAction
+              label="Search"
+              value="search"
+              icon={<SearchIcon sx={{ color: "white" }} />}
+            />
+          </NavLink>
+          <NavLink to="/user">
+            <BottomNavigationAction
+              label="Favorites"
+              value="favorites"
+              icon={<FavoriteIcon sx={{ color: "rgb(143,25,27)" }} />}
+            />
+          </NavLink>
+          {isLoggedIn ? (
+            <NavLink to="/landing">
+              <BottomNavigationAction
+                label="logout"
+                value="logout"
+                icon={<LogoutIcon sx={{ color: "white" }} />}
+                onClick={logout}
+              />
+            </NavLink>
+          ) : (
+            <NavLink to="/login">
+              <BottomNavigationAction
+                label="login"
+                value="login"
+                icon={<LoginIcon sx={{ color: "white" }} />}
+              />
+            </NavLink>
+          )}
+        </BottomNavigation>
       </Box>
     </div>
   );
