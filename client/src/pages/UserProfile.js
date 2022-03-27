17a97acc4bfa6b937ec0afd2f2ef230f806666fb
Luka Licheli <lukalicheli@gmail.com>
2022-03-27 10:55:51 -0700

    Added ternary function to favorites page

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,11 +1,53 @@
 import React from "react";
 import FavoriteCards from "../components/FavoriteCards";
+import { useAuth } from "../util/auth";
+import { NavLink } from "react-router-dom";
 
 export default function UserProfile() {
+  const { isLoggedIn, logout } = useAuth();
+
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Favorites</h1>
-      <FavoriteCards />
+      <h1 style={{ textAlign: "center", marginBottom: "30px" }}>Favorites</h1>
+   
+      {isLoggedIn ? (
+   <FavoriteCards />
+
+      ): (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <p
+            style={{
+              color: "black",
+              backgroundColor: "white",
+              padding: "10px",
+              width: "39ch",
+              textAlign: "center",
+              marginBottom: "30px"
+            }}
+          >
+            You need to be{" "}
+            <NavLink
+              to="/login"
+              style={{ color: "blue", textDecoration: "none" }}
+            >
+              logged in
+            </NavLink>{" "}
+            or{" "}
+            <NavLink
+              to="/signup"
+              style={{ color: "blue", textDecoration: "none" }}
+            >
+              sign up
+            </NavLink>{" "}
+            to save and view favorite restaurants!
+          </p>
+        </div>)}
+      
     </div>
   );
 }
